6869f4ab791afb8dd63f3649b14fcf072dd85a57
MaestroTool <51473403+MaestroTool@users.noreply.github.com>
2019-06-08 15:01:56 +0700

    Upload By Maestro

---
+++ Maestrov1.sh
@@ -0,0 +1,326 @@
+#usr/bin/bash
+clear
+#ini warna
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Recode ajh mmnx
+echo
+echo
+#ini paket untuk menjalankan tools
+DinoSec.sh
+echo
+clear
+echo
+echo
+echo
+echo $cy "[{•}]=============[About]=============[{•}]"
+echo $i  "AUTHOR    : Maestro"
+echo $i  "Facebook  : /xlucifer101"
+echo $i  "Team         : DinoSecurity404"
+echo $cy "[{•}]=============[About]=============[{•}]"
+echo
+echo
+sleep 5
+echo $me "===============[Tool Facebook]==============="
+echo $cy "[1]"$bi" Brute Force Facebook"
+echo $cy "[2]"$bi" Hack FB Target"
+echo $cy "[3]"$bi" Profil Guard FB"
+echo $cy "[4]"$bi" Auto Report FB"
+echo $cy "[5]"$bi" Auto Like FB"
+echo $cy "[6]"$bi" Auto BOT Komen FB"
+sleep 1
+echo "===============[Spammer]==============="
+echo $cy "[7]"$ku" Spam Grab"
+echo $cy "[8]"$ku" Spam TOPED"
+echo $cy "[9]"$ku" Spam Call"
+echo $cy "[10]"$ku" Spam SMS"
+echo $cy "[11]"$ku" Spam WA"
+sleep 1
+echo "===============[Tool 1]==============="
+echo $cy "[12]"$me" Skull Project"
+echo $cy "[13]"$me" Termux Alice"
+echo $cy "[14]"$me" Trap Project"
+echo $cy "[15]"$me" Mata Mata"
+sleep 1
+echo "===============[Tool 2]==============="
+echo $cy "[16]"$i" Sadap CCTV"
+echo $cy "[17]"$i" Install TUANBADUT"
+echo $cy "[18]"$i" Install Tool B4JINGANv6"
+echo $cy "[19]"$i" Install Ratusan Tool"
+sleep 1
+echo "===============[Tool 3]==============="
+echo $cy "[20]"$me" Install NetHunter"
+echo $cy "[21]"$me" Install Linux"
+echo $cy "[22]"$me" Install Ubuntu"
+echo $cy "[23]"$me" Install Parrot"
+sleep 1
+echo "==============[Tool 4]==============="
+echo $cy "[24]"$pu" HULK"
+echo $cy "[25]"$pu" Hammer"
+echo $cy "[26]"$pu" DDOS"
+sleep 1
+echo "===============[Tool Phising]==============="
+echo $cy "[27]"$i" SocialPhish"
+echo $cy "[28]"$i" Weeman"
+echo "======================================"
+echo $cy "[0]"$me" EXIT"
+echo
+echo
+echo $cy"==="$bi"[ ©2019 ]"$cy"==="
+read -p"root@DinoSecurity: ~# " pil;
+
+if [ $pil = 1 ]
+then
+clear
+pkg install php $y
+pkg install git $y
+pkg install python2 $y
+git clone https://github.com/FR13ND8/BRUTEFORCEnew.git
+cd BRUTEFORCEnew $y
+bash.new.sh $y
+fi
+
+if [ $pil = 2 ]
+then
+clear
+git clone https://github.com/zlucifer/troll_project.git
+cd troll_project $y
+bash.troll.sh $y
+fi
+
+if [ $pil = 3 ]
+then
+clear
+git clone https://github.com/xlucifercy/GuardProfilFB.git
+cd GuardProfilFB $y
+php GuardFB.php $y
+fi
+
+if [ $pil = 4 ]
+then
+clear
+git clone https://github.com/IlayTamvan/Report.git
+cd Report $y
+unzip Report.zip $y
+python2 Report.py $y
+fi
+
+if [ $pil = 5 ]
+then
+clear
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF $y
+python2 osif.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena $y
+python2 Botkomena.py $y
+fi
+
+if [ $pil = 7 ]
+then
+clear
+git clone https://github.com/MaestroTool/Spammer.git
+cd Spammer $y
+sh Spammer.sh $y
+fi
+
+if [ $pil = 8 ]
+then
+clear
+git clone https://github.com/MaestroTool/Spammer.git
+cd Spammer $y
+sh Spammer.sh $y
+fi
+
+if [ $pil = 9 ]
+then 
+clear
+git clone https://github.com/MaestroTool/Spammer.git
+cd Spammer $y
+sh Spammer.sh $y
+fi
+
+if [ $pil = 10 ]
+then
+clear
+git clone https://github.com/MaestroTool/Spammer.git
+cd Spammer $y
+sh Spammer.sh $y
+fi
+
+if [ $pil = 11 ]
+then
+clear
+git clone https://github.com/MaestroTool/Spammer.git
+cd  Spammer $y
+bash Spammer.sh $y
+fi
+
+if [ $pil = 12 ]
+then
+clear
+git clone https://github.com/zlucifer/skull_project.git
+cd skull_project $y
+bash skull.sh $y
+fi
+
+
+if [ $pil = 13 ]
+then
+clear
+git clone https://github.com/zlucifer/termux_alice.git
+cd termux_alice $y
+sh alice.sh $y
+fi
+
+if [ $pil = 14 ]
+then
+clear
+git clone https://github.com/zlucifer/trap_project.git
+cd trap_project $y
+bash trap.sh $y
+fi
+
+if [ $pil = 15 ]
+then
+clear
+git clone https://github.com/MaestroTool/DinoSec404.git
+cd DinoSec404 $y
+sh DinoSec.sh $y
+fi
+
+
+if [ $pil = 16 ]
+then
+clear
+git clone https://github.com/kancotdiq/ipcs.git
+cd ipcs $y
+python2 scan.py $y
+fi
+
+if [ $pil = 17 ]
+then
+clear
+pkg install python python2 vim figlet curl $y
+pkg install php $y
+pkg install lolcat
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4.git
+cd TOOLSINSTALLERv4 $y
+chmod 777 TUANB4DUT..sh $y
+./TUANB4DUT..sh $y
+fi
+
+if [ $pil = 18 ]
+then
+clear
+pkg install termcolor $y
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6.git
+cd BAJINGANv6 $y
+sh BAJINGAN.sh $y
+fi
+
+if [ $pil = 19 ]
+then
+clear
+git clone https://github.com/Ranginang67/DarkFly-Tool.git
+cd DarkFly-Tool $y
+python2 install.py $y
+fi
+
+
+if [ $pil = 20 ]
+then
+clear
+git clone https://github.com/Hax4us/Nethunter-In-Termux.git
+cd Nethunter-In-Termux $y
+chmod +x * $y
+./kalinethunter $y
+./startkali.sh $y
+fi
+
+if [ $pil = 21 ]
+then
+clear
+pkg install wget proot $y
+wget https://bulid.nethunter.com/kalifs/kalifs-20171013/kalifs-amd64-minimal.tar.xz $y
+proot -link2symlink tar -xf kalifs-amd64-minimal.tar.xz $y
+fi
+
+
+if [ $pil = 22 ]
+then
+clear
+git clone https://github.com/Neo-Oil/termux-ubuntu.git
+cd termux-ubuntu $y
+chmod +x ubuntu.sh $y
+./ubuntu.sh $y
+./start-ubuntu.sh $y
+fi
+
+
+if [ $pil = 23 ]
+then
+clear
+pkg install wget proot -y $$ wget https://guthubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Parrot/parrot.sh $y
+bash parrot.sh $y
+./start-parrot.sh $y
+fi
+
+if [ $pil = 24 ]
+then
+clear
+git clone https://github.com/grafov/hulk.git
+cd hulk $y
+fi
+
+if [ $pil = 25 ]
+then
+clear
+git clone https://github.com/cyweb/hammer.git
+cd hammer $y
+python2 hammer.py $y
+fi
+
+
+if [ $pil = 26 ]
+then
+clear
+git clone https://github.com/cyweb/hammer.git
+cd hammer $y
+python2 hammer.py $y
+fi
+
+if [ $pil = 27 ]
+then
+clear
+git clone https://github.com/An0nUD4Y/SocialFish.git
+cd SocialFish $y
+python2 SocialFish.py $y
+fi
+
+if [ $pil = 28 ]
+then
+clear
+pip2 install beautifulsoup4 $y
+git clone https://github.com/samyoyo/weeman.git
+cd weeman $y
+python2 weeman.py $y
+fi
+
+if [ $pil = 0 ]
+then
+clear
+sleep 2
+echo $cy"Terima Kasih Sudah Pakai Tool Saya"
+exit
+fi
